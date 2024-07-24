a7f8be45d8638a29aae349e122c63e8ab3e0a26e
ahmed ejaz <ahm.ejaz791@gmail.com>
2024-07-24 20:05:26 +0500

    section 4 changes

--- src/component/homepage-section4.jsx
+++ src/component/homepage-section4.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/body.scss'
 
 function HomepageSection4() {
+  const [videoStatus, setVideoStatus] = useState(false);
+  const videoSrc = "https://www.youtube.com/embed/93HEp2yS2oc?si=sqzjTRpdp85unNna"
+  const video=<iframe width="736" height="523" style={{borderRadius: 20}} src={videoSrc} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>;
   return (
     <div className='homepage-section-4 d-flex align-items-center justify-content-center'>
         <div className="homepage-section-4-left">
@@ -10,10 +13,17 @@ function HomepageSection4() {
             <p>Throughout the Passover season, Masbia <br /> expects to distribute 10,000 raw food packages, <br /> which will include special holiday staples for <br /> families to be able to prepare their own Seder <br /> and Kosher for Passover</p>
         </div>
         <div className="homepage-section-4-right">
-            <div className='homepage-section-4-right-image-container position-relative'>
-                <img src="/assets/video-image.png" alt="video-image" className='homepage-section-4-right-video-image' />
-                <img src="/assets/play-btn-icon.png" alt="play-btn-icon" className='homepage-section-4-right-play-btn position-absolute' />
-            </div>
+              {
+                videoStatus ?
+                <div>
+                  {video}
+                </div>
+                :
+                <div className='homepage-section-4-right-image-container position-relative'>
+                  <img src="/assets/video-image.png" alt="video-image" className='homepage-section-4-right-video-image' />
+                  <img src="/assets/play-btn-icon.png" alt="play-btn-icon" onClick={() => setVideoStatus(true)} className='homepage-section-4-right-play-btn position-absolute' />
+                </div>
+              }
         </div>
     </div>
   )
