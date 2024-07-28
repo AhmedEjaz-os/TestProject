f17318d7caadc0d443bbf1900f01857366d0ee84
ahmed ejaz <ahm.ejaz791@gmail.com>
2024-07-29 00:55:04 +0500

    button hovers + spacing fixes

--- src/component/homepage-section5.jsx
+++ src/component/homepage-section5.jsx
@@ -4,9 +4,9 @@ import '../styles/body.scss'
 function HomepageSection5() {
   return (
     <div className='homepage-section-5 d-flex align-items-center justify-content-center flex-column'>
-        <img src="/assets/section-4-icon.png" alt="section-4-icon" />
+        <img className='homepage-section-5-top-icon' src="/assets/section-4-icon.png" alt="section-4-icon" />
         <h1><b>Get</b> Involved</h1>
-        <p className='text-center'>Throughout the Passover season, Masbia expects to distribute 10,000 raw food <br /> packages, which will include special holiday staples for families to be able to <br /> prepare their own Seder and Kosher for Passover </p>
+        <p className='text-center mb-0'>Throughout the Passover season, Masbia expects to distribute 10,000 raw food <br /> packages, which will include special holiday staples for families to be able to <br /> prepare their own Seder and Kosher for Passover </p>
         <div className='homepage-section-5-btn-container'>
           <button className='btn text-uppercase me-4 homepage-section-5-btn-container-btn-1'>Donate</button>
           <button className='btn text-uppercase homepage-section-5-btn-container-btn-2'>Get Food</button>
